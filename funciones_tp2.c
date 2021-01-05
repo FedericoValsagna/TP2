1ad43cf52b6c650c703d41e97cd036104ccfcd84
Joaquin Prada <joaquiprada@gmail.com>
2021-01-05 00:59:17 -0300

    Agregado funciones_tp2.c y .h

---
+++ funciones_tp2.c
@@ -0,0 +1,13 @@
+#include "funciones_tp2.h"
+
+bool Pedir_Turno(paciente_t* paciente){
+
+}
+
+bool atender_siguiente_paciente(doctor_t* doctor){
+
+}
+
+bool informe_doctores(char letra_inicio, char letra_final){
+
+}
---
+++ funciones_tp2.h
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+
+"Se recibe un nombre de paciente y el nombre de una especialidad,
+ y el sistema le añade a la lista de espera de la especialidad correspondiente."
+bool Pedir_Turno(paciente_t* paciente);
+
+"Se recibe el nombre de le doctore que quedó libre,
+ y este atiende al siguiente paciente urgente (por orden de llegada).
+ Si no hubiera ningún paciente urgente,
+ atiende al siguiente paciente con mayor antigüedad como paciente en la clínica."
+bool atender_siguiente_paciente(doctor_t* doctor)
+
+
+"El sistema imprime la lista de doctores en orden alfabético,
+ junto con su especialidad y el número de pacientes que atendieron desde que arrancó el sistema.
+ Opcionalmente, se puede especificar el rango (alfabético) de doctores sobre los que se desean informes."
+bool informe_doctores(char letra_inicio, char letra_final);
